1074c13873f43064f7330f86f36991e6b09df4ec
Demi Yu <demiyuhongjun@gmail.com>
2017-05-09 17:09:27 +0800

    2017-05-09

---
+++ script1.py
@@ -0,0 +1,10 @@
+#!
+# A first Python script
+
+import sys # Load a library module
+print(sys.platform)
+print(2 ** 100) #Raise 2 to a power
+x = 'Spam!'
+print(x * 8) #String repetition
+
+print(2 ** 2)
