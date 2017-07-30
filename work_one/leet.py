931f0cc491ecc0bc25377820f627ef7ca9900f61
Demi Yu <demiyuhongjun@gmail.com>
2017-07-30 19:14:27 +0800

    leet

--- work_one/leet.py
+++ work_one/leet.py
@@ -4,26 +4,48 @@
 __author__ = 'Demi Yu'
 
 
+# class Solution(object):
+#     def longestCommonPrefix(self, strs):
+#         """
+#         :type strs: List[str]
+#         :rtype: str
+#         """
+#         if not strs:
+#             print("kong")
+#             return ""
+#         for i, letter_group in enumerate(zip(*strs)):
+#             print(list(zip(*strs)))
+#             print(len(set(letter_group)))
+#             if len(set(letter_group)) > 1:
+#                 print(i)
+#                 print('jinlaile')
+#                 print(strs[0][:i])
+#                 return strs[0][:i]
+#             else:
+#                 print('min')
+#                 print(min(strs))
+#                 return min(strs)
+# # me=Solution()
+# Solution().longestCommonPrefix(["abc","de"])
+
 class Solution(object):
-    def longestCommonPrefix(self, strs):
+    def isValid(self, s):
         """
-        :type strs: List[str]
-        :rtype: str
+        :type s: str
+        :rtype: bool
         """
-        if not strs:
-            print("kong")
-            return ""
-        for i, letter_group in enumerate(zip(*strs)):
-            print(list(zip(*strs)))
-            print(len(set(letter_group)))
-            if len(set(letter_group)) > 1:
-                print(i)
-                print('jinlaile')
-                print(strs[0][:i])
-                return strs[0][:i]
+        stack = []
+        dict = {"]":"[", "}":"{", ")":"("}
+        for char in s:
+            if char in dict.values():
+
+                stack.append(char)
+                print(stack)
+            elif char in dict.keys():
+                if stack == [] or dict[char] != stack.pop():
+                    return False
             else:
-                print('min')
-                print(min(strs))
-                return min(strs)
-# me=Solution()
-Solution().longestCommonPrefix(["abc","de"])
+                return False
+        return stack == []
+result=Solution().isValid("{}[")
+print(result)
