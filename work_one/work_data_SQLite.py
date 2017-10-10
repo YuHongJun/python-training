b33c47ef44be9192562a6f02ed7d55897c9f90d4
Demi Yu <demiyuhongjun@gmail.com>
2017-10-10 16:42:18 +0800

    code

---
+++ work_one/work_data_SQLite.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+import os, sqlite3
+
+db_file = os.path.join(os.path.dirname(__file__), 'test.db')
+if os.path.isfile(db_file):
+    os.remove(db_file)
+
+# 初始数据:
+conn = sqlite3.connect(db_file)
+cursor = conn.cursor()
+cursor.execute('create table user(id varchar(20) primary key, name varchar(20), score int)')
+cursor.execute(r"insert into user values ('A-001', 'Adam', 95)")
+cursor.execute(r"insert into user values ('A-002', 'Bart', 62)")
+cursor.execute(r"insert into user values ('A-003', 'Lisa', 78)")
+cursor.close()
+conn.commit()
+conn.close()
